d47d3877359f306a6827aaf473119fbd8450cd45
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-27 23:08:38 +0530

    ❇️ update create Tweet Resolver

--- packages/api/src/resolvers/tweets.ts
+++ packages/api/src/resolvers/tweets.ts
@@ -1,5 +1,7 @@
 import { Tweet } from '../entities/Tweet';
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
+import { IContext } from '../utils/types';
+import { User } from '../entities/User';
 
 @Resolver()
 export class TweetResolvers {
@@ -10,12 +12,21 @@ export class TweetResolvers {
 
   @Mutation(() => Tweet)
   async createTweet(
-    @Arg('username') username: string,
-    @Arg('body') body: string
+    @Arg('media', { nullable: true }) media: string,
+    @Arg('body') body: string,
+    @Ctx() { authUser }: IContext
   ): Promise<Tweet> {
+    if (!authUser) {
+      throw new Error('NOT AUTHENTICATED');
+    }
+    const { id } = authUser;
+    const user = await User.findOne(id);
+    if (!user) throw new Error('User Not found!');
     const tweet = await Tweet.create({
       body,
-      username,
+      has_media: media ? true : false,
+      author: user,
+      media,
     }).save();
 
     return tweet;
