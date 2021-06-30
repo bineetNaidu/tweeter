1fcd005f835133ee33da051e4e1476302bff8b47
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-30 12:47:51 +0530

    ✨ add update & delete comment mutations

--- packages/api/src/resolvers/comments.ts
+++ packages/api/src/resolvers/comments.ts
@@ -4,6 +4,7 @@ import { Arg, Ctx, Int, Mutation, Resolver, UseMiddleware } from 'type-graphql';
 import { Comment } from '../entities/Comment';
 import { Tweet } from '../entities/Tweet';
 import { User } from '../entities/User';
+import { getConnection } from 'typeorm';
 
 @Resolver()
 export class CommentResolver {
@@ -26,5 +27,53 @@ export class CommentResolver {
     }).save();
 
     return comment;
+  }
+
+  @Mutation(() => Comment, { nullable: true })
+  @UseMiddleware(isAuthed)
+  async updateComment(
+    @Arg('id', () => Int) id: number,
+    @Arg('text') text: string,
+    @Ctx() { authUser }: IContext
+  ): Promise<Comment | null> {
+    try {
+      const result = await getConnection()
+        .createQueryBuilder()
+        .update(Comment)
+        .set({
+          text,
+        })
+        .where('id = :id and "userId" = :userId', {
+          id,
+          userId: authUser!.id,
+        })
+        .returning('*')
+        .execute();
+      return result.raw[0];
+    } catch (err) {
+      return null;
+    }
+  }
+
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuthed)
+  async deleteComment(
+    @Arg('id', () => Int) id: number,
+    @Ctx() { authUser }: IContext
+  ): Promise<boolean> {
+    try {
+      const comment = await Comment.findOne({
+        where: {
+          id,
+          user: authUser!.id,
+        },
+      });
+      if (!comment) throw new Error('Comment Was Not found!');
+      await comment.remove();
+
+      return true;
+    } catch (err) {
+      return false;
+    }
   }
 }
