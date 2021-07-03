89c5586b14ada39ab592e61ab8b9d5bd0e4e1408
bineetNaidu <bineet.s.naidu@gmail.com>
2021-07-03 22:29:33 +0530

    🎉 add filter tweets state w/ UI

--- packages/web/src/pages/[username].tsx
+++ packages/web/src/pages/[username].tsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { Navbar } from '../components/Navbar';
 import { useUserQuery } from '../generated/graphql';
 import { withApollo } from '../lib/withApollo';
 import { Spin, Avatar, Button } from 'antd';
 import { UserAddOutlined } from '@ant-design/icons';
+import { UserTweetsContainer } from '../components/UserTweetsContainer';
+import { UserLikeTweetsContainer } from '../components/UserLikeTweetContainer';
+import { UserMediaTweetsContainer } from '../components/UserMediaTweetContainer';
 import styles from '../styles/users.module.scss';
+
+type FilterType = 'TWEETS' | 'MEDIA_TWEETS' | 'LIKE_TWEETS';
 
 const Users = () => {
   const fallbackImg =
@@ -13,6 +19,9 @@ const Users = () => {
   const { data, loading } = useUserQuery({
     variables: { username: (r.query.username as string).replace('@', '') },
   });
+
+  const [filterType, setFilterType] = useState<FilterType>('TWEETS');
+
   return (
     <div>
       <Navbar />
@@ -53,6 +62,49 @@ const Users = () => {
                 </Button>
               </div>
               <p className={styles.bio}>{data.user.bio ?? 'No Bio!'}</p>
+            </div>
+          </div>
+          <div className={styles.tweets}>
+            <div className={styles.filter_grid}>
+              <ul>
+                <li
+                  onClick={() => setFilterType('TWEETS')}
+                  className={
+                    filterType === 'TWEETS' ? styles.active : undefined
+                  }
+                >
+                  Tweets
+                </li>
+                <li>Tweets & Replies</li>
+                <li
+                  onClick={() => setFilterType('MEDIA_TWEETS')}
+                  className={
+                    filterType === 'MEDIA_TWEETS' ? styles.active : undefined
+                  }
+                >
+                  Media
+                </li>
+                <li
+                  onClick={() => setFilterType('LIKE_TWEETS')}
+                  className={
+                    filterType === 'LIKE_TWEETS' ? styles.active : undefined
+                  }
+                >
+                  Likes
+                </li>
+              </ul>
+            </div>
+
+            <div className={styles.tweets_grid}>
+              {filterType === 'TWEETS' ? (
+                <UserTweetsContainer username={data.user.username} />
+              ) : null}
+              {filterType === 'LIKE_TWEETS' ? (
+                <UserLikeTweetsContainer username={data.user.username} />
+              ) : null}
+              {filterType === 'MEDIA_TWEETS' ? (
+                <UserMediaTweetsContainer username={data.user.username} />
+              ) : null}
             </div>
           </div>
         </div>
